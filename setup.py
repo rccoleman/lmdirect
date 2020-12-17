c5e6d11d50df1b733f48319358630894d6340546
rccoleman <rccoleman@gmail.com>
2020-12-16 22:28:45 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+with open('README.rst', 'r') as f:
+    readme = f.read()
+
+with open('HISTORY.rst', 'r') as f:
+    history = f.read()
+
+setup(
+    name='lmdirect',
+    version='v0.1',
+    description='A Python implementation of the local La Marzocco API',
+    long_description=readme + '\n\n' + history,
+    url='https://github.com/rccoleman/lmdirect',
+    author='Rob Coleman',
+    author_email='rccoleman@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 1 - Experimental',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    packages=['lmdirect'],
+    install_requires=[
+        'pycryptodomex>=3.9.9; python_version > "3.8"'
+    ],
+    package_data={'license': ['LICENSE'],},
+)
