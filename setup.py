203aa2e42e6751480cbd48cce83bd24cf049985a
Rob Coleman <rccoleman@gmail.com>
2022-05-28 13:47:15 -0700

    Squashed commit of the following:
    commit 27eee16137f80d1d16a88426803d19d3c97a5a78
    Author: Tim Clipsham <tim.clipsham@gmail.com>
    Date:   Mon Dec 13 17:12:56 2021 +1100
    
        Bump library version to 0.8.0
    
    commit 46d6882c63c81bba4cf0c1e0fc8cb955faeeb7cf
    Author: Tim Clipsham <tim.clipsham@gmail.com>
    Date:   Sun Dec 12 01:09:23 2021 +1100
    
        Add message type for water reservoir contact
    
    commit ca6e173f2f58717e63448c4611e10363d42f64a4
    Author: Tim Clipsham <tim.clipsham@gmail.com>
    Date:   Sat Dec 11 22:37:54 2021 +1100
    
        Add detection for water reservoir
    
    commit 11f42581c79f7d5f41e97cdb8f1b9b99b5667bb8
    Merge: 9b67eba afc09ae
    Author: rccoleman <rccoleman@gmail.com>
    Date:   Sat Oct 23 09:37:47 2021 -0700
    
        Merge pull request #32 from rccoleman/dev
    
        Updated requirements to avoid beta 1.x branch of authlib
    
    commit 9b67eba5cc073d6bbaaf82d970a9929ab3be118c
    Merge: 70fd4e8 bdfb0a1
    Author: rccoleman <rccoleman@gmail.com>
    Date:   Sat Oct 23 09:19:51 2021 -0700
    
        Merge pull request #31 from rccoleman/dev
    
        Bump to 0.15.5 and refactor connection code

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="lmdirect",
-    version="0.7.2",
+    version="0.8.0",
     description="A Python implementation of the local La Marzocco API",
     long_description=readme,
     long_description_content_type="text/markdown",
