54cd11cd3ddca5499a864855faf3bcac7644beca
Rob Coleman <rccoleman@gmail.com>
2022-10-31 10:23:46 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="lmdirect",
-    version="0.9.0",
+    version="0.9.1",
     description="A Python implementation of the local La Marzocco API",
     long_description=readme,
     long_description_content_type="text/markdown",
